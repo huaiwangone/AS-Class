2e3c9a4b661e854cebd2de3fc33f1c04b7e664df
huaiwangone <821747705@qq.com>
2021-04-09 10:47:23 +0800

    第五周

---
+++ app/src/main/java/com/one/points/MainActivity.java
@@ -0,0 +1,95 @@
+package com.one.points;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button pointA3=(Button) findViewById(R.id.buttonA3);
+        pointA3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointA = (TextView) findViewById(R.id.pointA);
+                int pointa = Integer.parseInt(pointA.getText().toString());
+                pointa +=3;
+                pointA.setText(String.valueOf(pointa));
+            }
+        });
+
+        Button pointA2=(Button) findViewById(R.id.buttonA2);
+        pointA2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointA = (TextView) findViewById(R.id.pointA);
+                int pointa = Integer.parseInt(pointA.getText().toString());
+                pointa +=2;
+                pointA.setText(String.valueOf(pointa));
+            }
+        });
+
+        Button pointA1=(Button) findViewById(R.id.buttonA1);
+        pointA1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointA = (TextView) findViewById(R.id.pointA);
+                int pointa = Integer.parseInt(pointA.getText().toString());
+                pointa +=1;
+                pointA.setText(String.valueOf(pointa));
+            }
+        });
+
+        Button pointB3=(Button) findViewById(R.id.buttonB3);
+        pointB3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointB = (TextView) findViewById(R.id.pointB);
+                int pointb = Integer.parseInt(pointB.getText().toString());
+                pointb +=3;
+                pointB.setText(String.valueOf(pointb));
+            }
+        });
+
+        Button pointB2=(Button) findViewById(R.id.buttonB2);
+        pointB2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointB = (TextView) findViewById(R.id.pointB);
+                int pointb = Integer.parseInt(pointB.getText().toString());
+                pointb +=2;
+                pointB.setText(String.valueOf(pointb));
+            }
+        });
+
+        Button pointB1=(Button) findViewById(R.id.buttonB1);
+        pointB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointB = (TextView) findViewById(R.id.pointB);
+                int pointb = Integer.parseInt(pointB.getText().toString());
+                pointb +=1;
+                pointB.setText(String.valueOf(pointb));
+            }
+        });
+
+        Button reset=(Button) findViewById(R.id.button7);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView pointA = (TextView) findViewById(R.id.pointA);
+                TextView pointB = (TextView) findViewById(R.id.pointB);
+                pointA.setText("0");
+                pointB.setText("0");
+            }
+        });
+
+    }
+}
